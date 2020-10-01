2e1d68973dfaa2e7dbf6adf22541966b1a9db47c
aj1511 <58018867+aj1511@users.noreply.github.com>
2020-10-01 10:32:38 +0530

    Add files via upload

---
+++ anagram.java
@@ -0,0 +1,38 @@
+	
+	public class anagram {
+	
+		public static void main(String[] args) {
+			
+			String a="listen";
+			String b="silent";
+			int al[]= new int[256];
+			boolean isAnagram=true;
+			
+			
+			for (char j:a.toCharArray())
+			{
+				int index = (int)j;
+				al[index]++;
+			}
+			
+			for (char j:b.toCharArray())
+			{
+				int index = (int)j;
+				al[index]--;
+			}
+			
+			for (int i=0;i<256;i++)
+			{
+				if(al[i]!=0)
+					{isAnagram=false;
+					break;
+					}
+			}
+			
+			if(isAnagram)
+				System.out.println("Anagram");
+			else
+				System.out.println("Not a Anagram");
+			
+		}		
+}
